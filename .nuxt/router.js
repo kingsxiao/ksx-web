c7b7b3d95eed4527c27c9a35a2d70e400d1e76be
wangxiao <1379655847@qq.com>
2022-08-08 17:08:02 +0800

    初始化项目

---
+++ .nuxt/router.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _2f3c1da2 = () => interopDefault(import('..\\pages\\home\\index.vue' /* webpackChunkName: "pages/home/index" */))
+const _8cdb5c4c = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages/user/login" */))
+const _2e977e7c = () => interopDefault(import('..\\pages\\user\\register.vue' /* webpackChunkName: "pages/user/register" */))
+const _1a91b424 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/home",
+    component: _2f3c1da2,
+    name: "home"
+  }, {
+    path: "/user/login",
+    component: _8cdb5c4c,
+    name: "user-login"
+  }, {
+    path: "/user/register",
+    component: _2e977e7c,
+    name: "user-register"
+  }, {
+    path: "/",
+    component: _1a91b424,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
---
+++ pages/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  layout: 'user-frame'
+}
+</script>
+
+<style scoped>
+
+</style>
